f60585eba220d241c01bc2b27722456b5015de12
Yang-Hsing Lin <yanghsing.lin@thekono.com>
2013-08-25 19:37:45 +0800

    add fadeInWhenImageLoaded directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,20 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('myApp.directives', [])
+  .config(function($provide) {
+    $provide.value("$", window.$);
+  })
+  .directive("fadeInAfterImageLoaded", function($) {
+    return {
+      link: function(scope, element, attr) {
+        scope.$on("currentAlbumChanged", function() {
+          $(element).hide();
+          $(element).imagesLoaded(function() {
+            $(element).fadeIn();
+          });
+          
+        });
+      }
+    }
+  });
--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -3,17 +3,51 @@
 /* jasmine specs for directives go here */
 
 describe('directives', function() {
-  beforeEach(module('myApp.directives'));
+  var mock$, mockElement, tag, scope;
 
-  describe('app-version', function() {
-    it('should print current version', function() {
-      module(function($provide) {
-        $provide.value('version', 'TEST_VER');
-      });
-      inject(function($compile, $rootScope) {
-        var element = $compile('<span app-version></span>')($rootScope);
-        expect(element.text()).toEqual('TEST_VER');
-      });
+  beforeEach(module('myApp.directives'));
+  beforeEach(function(){
+    module(function($provide) {
+      mockElement = {
+        hide: jasmine.createSpy("hide")
+        , imagesLoaded: jasmine.createSpy("imagesLoaded")
+        , fadeIn: jasmine.createSpy("fadeIn")
+      };
+      mock$ = function() {
+        return mockElement;
+      };
+      $provide.value("$", mock$);
     });
+  });
+
+  beforeEach(inject(function($rootScope){
+    scope = $rootScope.$new();
+    tag = "<div fade-in-after-image-loaded></div>";  
+  }));
+  
+  describe("$on 'currentAlbumChanged'", function(){
+    it("hides the element", inject(function($compile){
+      $compile(tag)(scope);
+      scope.$broadcast("currentAlbumChanged");
+
+      expect(mockElement.hide).toHaveBeenCalled();
+    }));
+
+    it("registers a loaded callback using 'imagesLoaded' to fadeIn the element"
+      , inject(function($compile){
+    
+      var onLoad;
+      mockElement.imagesLoaded = function(cb) {
+        onLoad = cb;
+      };
+      spyOn(mockElement, "imagesLoaded").andCallThrough();
+
+      $compile(tag)(scope);
+      scope.$broadcast("currentAlbumChanged");
+      onLoad();
+
+      expect(mockElement.imagesLoaded).toHaveBeenCalled();
+      expect(mockElement.fadeIn).toHaveBeenCalled();
+    }));
   });
 });
