8b83b4b6fbc3cdf5a57b8706d176687ef1ca6426
Yang-Hsing Lin <yanghsing.lin@thekono.com>
2013-08-25 15:09:13 +0800

    sends request to fetch albums in AlbumsController

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,8 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('AlbumsController', function() {
-
+  controller('AlbumsController', function($scope, $http) {
+    $http.get("/assets/jsons/albums.json").success(function(albums) {
+      $scope.albums = albums;
+    });
   });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,6 +2,49 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
+describe('AlbumsController', function(){
+  var http, scope, controller;
+  var mockAlbumns = []
+    , albumUrl = "/assets/jsons/albums.json";
+
   beforeEach(module('myApp.controllers'));
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+    http = _$httpBackend_;
+    scope = $rootScope.$new();
+    controller = $controller;
+  }));
+
+  beforeEach(function(){
+    http.whenGET(albumUrl).respond(mockAlbumns);
+  });
+  
+
+  afterEach(function() {
+    http.verifyNoOutstandingRequest();
+    http.verifyNoOutstandingExpectation();
+  })
+
+  function createController () {
+    controller("AlbumsController", {
+      $scope: scope
+    });
+  }
+
+  it("sends request to server to fetch albums data", function(){
+    http.expectGET(albumUrl).respond(mockAlbumns);
+
+    createController();
+    http.flush();
+  });
+
+  it("sets albumns to $scope.albums after fetching success", function(){
+    createController();
+    http.flush();
+
+    expect(scope.albums).toEqual(mockAlbumns);
+  });
+  
+  
+  
+  
 });
